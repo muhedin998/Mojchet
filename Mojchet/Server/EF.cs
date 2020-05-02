5d976d5053f0272469aa9729301a907e4d0386ff
Muhedin Alic <muhedin998@gmail.com>
2020-05-02 15:52:32 +0200

    Add project files.

---
+++ Mojchet/Server/EF.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mojchet.Shared;
+
+namespace Mojchet.Server
+{
+    public class EF : DbContext
+    {
+        public DbSet<People> Peoples {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<People>().HasKey(u => u.ID);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-9NEAIKT;Initial Catalog=ChatBaza;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ Mojchet/Shared/People.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mojchet.Shared
+{
+    public class People
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Pasword { get; set; }
+        public string Name { get; set; }
+
+        public People() { }
+        public People(string usrn,string psw, string nme )
+        {
+            Username = usrn;
+            Pasword = psw;
+            Name = nme;
+        }
+        public People(string usrn, string psw)
+        {
+            Username = usrn;
+            Pasword = psw;
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+    }
+}
